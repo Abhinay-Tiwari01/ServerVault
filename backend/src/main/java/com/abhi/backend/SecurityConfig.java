fd942a2f75de08b4bb4f33fd93bf275dfe2d07e8
Abhinay-Tiwari01 <tabhinay3@gmail.com>
2026-05-20 10:12:44 +0530

    Update SecurityConfig.java

--- backend/src/main/java/com/abhi/backend/SecurityConfig.java
+++ backend/src/main/java/com/abhi/backend/SecurityConfig.java
@@ -23,9 +23,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/login/**").permitAll()
                                 .requestMatchers("/api/server/**").permitAll()
-
-//                        .requestMatchers("/api/getAllUsers      ").permitAll()
-//                        .requestMatchers("/api/login/deleteById/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
@@ -38,7 +35,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500",
+                "http://localhost:5500",
+                "https://servervault.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
@@ -46,32 +45,4 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", config);
         return source;
     }
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/login/**").permitAll()  // ← this is what you need
-//                        .requestMatchers("/api/users/**").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .sessionManagement(session -> session
-//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                );
-//
-//        return http.build();
-//    }
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.addAllowedOrigin("http://localhost:5500");
-//        config.addAllowedOrigin("http://127.0.0.1:5500");
-//        config.addAllowedMethod("*");
-//        config.addAllowedHeader("*");
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/api/login/**", config);
-//        return source;
-//    }
 }
